9c442709d0ca0eaccc3946fb54ccf4fdcb077677
vvdam <maaalouladam@gmail.com>
2024-04-22 10:37:53 +0200

    affiche card

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import CardList from './pages/CardList.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import CardList from "./pages/CardList.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
+    {
         path: "/",
-        element: <CardList />
-      }
-    ]
-  }
-])
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <CardList />,
+                loader: () =>
+                    fetch(
+                        "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=30&offset=0"
+                    ),
+            },
+        ],
+    },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
--- src/pages/CardList.jsx
+++ src/pages/CardList.jsx
@@ -1,21 +1,32 @@
 import SearchBar from "../components/SearchBar";
 import RaceFilter from "../components/RaceFilter";
+import { useLoaderData } from "react-router-dom";
 // import Card component here
 
 function CardList() {
-  
-  // get data from API here
+    const results = useLoaderData();
 
-  return (
-    <main>
-      <aside>
-        <h3>Apply filters</h3>
-        <SearchBar />
-        <RaceFilter />
-      </aside>
-      {/* map cards here */}
-    </main>
-  );
+    // get data from API here
+    console.log(results);
+    console.log(results.data[1].card_images[0].image_url);
+
+    return (
+        <main>
+            <aside>
+                <h3>Apply filters</h3>
+                <SearchBar />
+                <RaceFilter />
+            </aside>
+            {results.data.map((card, index) => {
+                return (
+                    <img
+                        key={card[index]}
+                        src={card.card_images[0].image_url}
+                        height={250}
+                    />
+                );
+            })}
+        </main>
+    );
 }
-
 export default CardList;
